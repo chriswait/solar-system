e90676a6f94bb414ade4a33a92b42671454790f6
chriswait <chriswait91@gmail.com>
2018-02-24 23:44:35 +1100

    adds vue linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,17 @@ module.exports = {
     "es6": true,
     "jasmine": true,
   },
-  extends: "eslint:recommended",
-  parser: 'babel-eslint',
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/recommended"
+  ],
   parserOptions: {
+    parser: 'babel-eslint',
     sourceType: 'module'
   },
   rules: {
-    "no-console": 0
+    "no-console": 0,
+    "semi": ["error", "never"],
+    "no-multiple-empty-lines": ["error", { "max": 1}]
   }
 };
