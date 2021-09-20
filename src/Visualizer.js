59c2c7f8a0285636b3c580cc1f0a94ac9529d919
chriswait <chriswait91@gmail.com>
2021-09-20 01:33:14 +0100

    rebuild Visualiser using react-three-fiber

--- src/Visualizer.js
+++ src/Visualizer.js
@@ -1,79 +1,151 @@
-import React, { useState, useRef, useCallback, useEffect } from "react";
+import React, { Suspense, useRef, useState, useCallback } from "react";
 
-import { Screen } from "./screen";
+import { BackSide, Vector3 } from "three";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { PerspectiveCamera, OrbitControls } from "@react-three/drei";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
+
 import { useSolarSystem } from "./SolarSystemProvider";
-import { OrbitControls } from "./orbit-controls";
-import { FRAME_RATE } from "./constants";
 import OverlayObject from "./OverlayObject";
 
+import { auToMeters, degreesToRadians } from "./util";
+import StarField from "./images/starfield.png";
+import { ORBIT_POINTS } from "./constants";
+
+const NEAR = 0.01;
+const FAR = 3000;
+const VIEW_ANGLE = 45;
+const SPHERE_SEGMENTS = 16;
+const SPHERE_RINGS = 16;
+const ORBIT_MAX_UNITS = 500;
+const HOVER_COLOR = "white";
+
+const realToVisualised = ({ x, y, z }, scaleFactor, setTargetName) =>
+  new Vector3(x, z, y).multiplyScalar(scaleFactor);
+
+const Star = ({ object, scaleFactor }) => {
+  const radius = object.radius * 1000 * scaleFactor * 10;
+  return (
+    <mesh onClick={() => setTargetName(object.name)}>
+      <sphereGeometry
+        attach="geometry"
+        args={[radius, SPHERE_SEGMENTS, SPHERE_RINGS]}
+      />
+      <meshLambertMaterial attach="material" color="white" />
+      <pointLight color="white" intensity={1} distance={0} />
+    </mesh>
+  );
+};
+
+const Orbit = ({ object, scaleFactor }) => {
+  const points = [];
+  for (let i = 0; i < ORBIT_POINTS; i++) {
+    points.push(realToVisualised(object.lastOrbit[i], scaleFactor));
+  }
+  points.push(realToVisualised(object.lastOrbit[0], scaleFactor));
+  const orbitGeometryRef = useCallback((node) => {
+    if (node) {
+      node.setFromPoints(points);
+    }
+  });
+  return (
+    <line>
+      <bufferGeometry ref={orbitGeometryRef} />
+      <lineBasicMaterial color={object.color} />
+    </line>
+  );
+};
+
+const Planet = ({
+  object,
+  scaleFactor,
+  currentTargetMeshRef,
+  setTargetName,
+}) => {
+  const radius = object.radius * 1000 * scaleFactor * 1000;
+  return (
+    <mesh
+      position={realToVisualised(object.position, scaleFactor)}
+      onClick={() => setTargetName(object.name)}
+      ref={currentTargetMeshRef}
+    >
+      <sphereGeometry
+        attach="geometry"
+        args={[radius, SPHERE_SEGMENTS, SPHERE_RINGS]}
+      />
+      <meshLambertMaterial attach="material" args={[{ color: object.color }]} />
+    </mesh>
+  );
+};
+
+const Stars = () => {
+  const starMap = useLoader(TextureLoader, StarField);
+  return (
+    <mesh>
+      <sphereGeometry
+        attach="geometry"
+        args={[ORBIT_MAX_UNITS * 2, SPHERE_SEGMENTS, SPHERE_RINGS]}
+      />
+      <meshBasicMaterial attach="material" side={BackSide} map={starMap} />
+    </mesh>
+  );
+};
+
 const Visualizer = () => {
-  const [hasInitialised, setHasInitialised] = useState(false);
-  const {
-    objects,
-    lastObject,
-    currentTargetName,
-    setTargetName,
-  } = useSolarSystem();
-  // We need a stable reference to our screen and controls across renders
-  const screenRef = useRef(new Screen());
-  const controlsRef = useRef(null);
+  const { objects, lastObject, currentTargetName, setTargetName } =
+    useSolarSystem();
+  const cameraRef = useRef();
+  const currentTargetMeshRef = useRef();
 
-  const canvasRef = useCallback((node) => {
-    if (node !== null) {
-      const screen = screenRef.current;
-      screen.initRenderer(node);
-      screen.setScaleForOuterObject(lastObject);
-      controlsRef.current = new OrbitControls(screen.camera, node);
-      objects.forEach((object) => screen.drawObject(object));
-      setHasInitialised(true);
-    }
-  }, []);
-
-  // Render Loop
-  useEffect(() => {
-    if (hasInitialised) {
-      const renderInterval = setInterval(() => {
-        requestAnimationFrame(() => {
-          objects.forEach((object) => screenRef.current.redrawObject(object));
-          controlsRef.current.update();
-          screenRef.current.render();
-        });
-      }, 1000 / 60);
-      return () => {
-        clearInterval(renderInterval);
-      };
-    }
-  }, [hasInitialised, objects, lastObject]);
-
-  useEffect(() => {
-    screenRef.current.setCameraTarget(currentTargetName, controlsRef.current);
-  }, [currentTargetName]);
-
-  // Work out our 2d position for each object
-  const objectsWithPositions = objects.map((object) => ({
-    ...object,
-    position2D: screenRef.current.get2DCoordinateForObject3D(
-      screenRef.current.getMeshWithName(object.name)
-    ),
-  }));
+  const furthestOrbitMeters = auToMeters(
+    lastObject.orbit.keplerianElements.initial.semiMajorAxisAu
+  );
+  const scaleFactor = ORBIT_MAX_UNITS / furthestOrbitMeters;
 
   return (
     <div id="visualizer-container">
-      <canvas
-        id="solar-system-canvas"
-        ref={canvasRef}
-        onClick={() => {
-          if (screenRef.current.hoverObject) {
-            setTargetName(screenRef.current.hoverObject.userData.name);
-          }
-        }}
-        onMouseMove={({ clientX, clientY }) => {
-          screenRef.current.checkHoverObject(clientX, clientY);
-        }}
-      />
-      {objectsWithPositions.map((object) => (
-        <OverlayObject object={object} key={object.name} />
-      ))}
+      <Canvas>
+        <OrbitControls
+          camera={cameraRef.current}
+          target={currentTargetMeshRef.current?.position}
+        />
+        <PerspectiveCamera
+          makeDefault
+          fov={VIEW_ANGLE}
+          near={NEAR}
+          far={FAR}
+          ref={cameraRef}
+          position={[0, 70, 0]}
+        />
+        <Suspense fallback={<>Loading</>}>
+          <Stars />
+        </Suspense>
+        {objects.map((object) =>
+          object.star ? (
+            <Star
+              object={object}
+              key={object.name}
+              scaleFactor={scaleFactor}
+              setTargetName={setTargetName}
+            />
+          ) : (
+            <React.Fragment key={object.name}>
+              <Planet
+                object={object}
+                key={object.name}
+                scaleFactor={scaleFactor}
+                currentTargetMeshRef={
+                  object.name === currentTargetName
+                    ? currentTargetMeshRef
+                    : undefined
+                }
+                setTargetName={setTargetName}
+              />
+              <Orbit object={object} scaleFactor={scaleFactor} />
+            </React.Fragment>
+          )
+        )}
+      </Canvas>
     </div>
   );
 };
