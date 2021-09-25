86a5bdada6dfec4c034f0137cc5db4a6fd1d39bf
chriswait <chriswait91@gmail.com>
2021-09-25 01:01:06 +0100

    hide object labels when close

--- src/Visualiser/ObjectLabel.js
+++ src/Visualiser/ObjectLabel.js
@@ -1,21 +1,29 @@
 import React from "react";
 import { Html } from "@react-three/drei";
+import { useVisualiser } from "../VisualiserProvider";
 
-const ObjectLabel = ({ object, onClick }) => (
-  <Html
-    style={{
-      color: "white",
-      borderWidth: 2,
-      borderStyle: "solid",
-      borderColor: object.color,
-      borderRadius: 5,
-      backgroundColor: "black",
-      padding: 5,
-    }}
-    zIndexRange={[20, 0]}
-  >
-    <div onClick={onClick}>{object.name}</div>
-  </Html>
-);
+const ObjectLabel = ({ object, onClick }) => {
+  const { targetCameraRef, scaleFactor } = useVisualiser();
+  const radius = object.radius * 1000 * scaleFactor * 10;
+  const cameraDistance = targetCameraRef.current?.position.length() - radius;
+  return (
+    cameraDistance > 0.03 && (
+      <Html
+        style={{
+          color: "white",
+          borderWidth: 2,
+          borderStyle: "solid",
+          borderColor: object.color,
+          borderRadius: 5,
+          backgroundColor: "black",
+          padding: 5,
+        }}
+        zIndexRange={[20, 0]}
+      >
+        <div onClick={onClick}>{object.name}</div>
+      </Html>
+    )
+  );
+};
 
 export default ObjectLabel;
